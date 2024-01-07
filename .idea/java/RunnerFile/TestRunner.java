bb5e6791a13248880154eb04ea7ad83423f6d89d
RDawlekar <rkumartippiripati@gmail.com>
2024-01-07 16:39:02 +0530

    commit

--- .idea/java/RunnerFile/TestRunner.java
+++ .idea/java/RunnerFile/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/features/sample.feature",plugin = { "pretty",
+@CucumberOptions(features = "src/test/java/features/DragDrop.feature",plugin = { "pretty",
 
         "html:src/test/java/Reports/report1.html","json:src/test/java/Reports/report.json"},glue={"StepDefinationFile"},monochrome=true)
 public class TestRunner {
